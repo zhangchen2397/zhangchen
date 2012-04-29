f3133da84a973d37f2c905d4f6541f2a09b4c170
zhangchen2397 <zhangchen2397@126.com>
2012-04-29 23:43:53 +0800

    init

---
+++ code_fragment/getXHR.js
@@ -0,0 +1,18 @@
+var getXHR = function() {
+	var xhr = null;
+	try {
+		xhr = new XMLHttpRequest();
+	} catch( e ) {
+		var msxml = [ 'MSXML2.XMLHTTP.3.0', 'MSXML2.XMLHTTP', 'Microsoft.XMLTTP' ]; 
+		for ( var i = 0; i < msxml.length; i++ ) {
+			try {
+				xhr = new ActiveXObject( msxml[ i ] );
+				break;
+			} catch( e ) {
+				
+			}
+		}
+	}
+
+	return xhr;
+}
